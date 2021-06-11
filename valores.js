f677bb53a90fe523db6fc4320a575c76bcf70e97
CesarRomeroDev <cesar_romero.dev@outlook.com>
2021-06-11 11:41:00 -0500

    valores

---
+++ valores.js
@@ -0,0 +1,26 @@
+//VALORES PRIMITIVOS
+
+
+"julio cesar romero" //texto 
+
+//boolean
+true
+false
+
+//valores vacios // espacio reservado para despues agregar codigo //o errores en consola
+null
+undefined
+
+
+
+//VALORES NO PRIMITIVOS
+
+[1,2,3] //arrels
+
+{nombre: "julio"} //valor tipo objeto
+
+string = 'julio'
+typeof(string);   //typeof // para saber el valor de una variable o componente
+
+
+
